9ba08b807577319a7fcfbaa326b66b588b4dbd75
Qianjiachen55 <qianjiachen@happywater.xyz>
2022-05-14 01:59:13 +0800

    init(viper)

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/lib/pq v1.10.5
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/spf13/viper v1.11.0
 	github.com/stretchr/testify v1.7.1
 )
--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import (
 	"database/sql"
 	"github.com/Qianjiachen55/pgK8/api"
 	db "github.com/Qianjiachen55/pgK8/db/sqlc"
+	"github.com/Qianjiachen55/pgK8/util"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:123@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
+
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config,err := util.LoadConfig(".")
+	if err != nil{
+		log.Fatal("cannot load config:",err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	//defer conn.Close()
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
@@ -24,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil{
 		log.Fatal("cannot start server:",err)
 	}
