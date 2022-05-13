c8013b36f74d13df2d9eaa91895ded8dabf45ee3
Qianjiachen55 <qianjiachen@happywater.xyz>
2022-05-14 02:05:29 +0800

    update(main_test.go): use viper to config db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"github.com/Qianjiachen55/pgK8/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
@@ -11,14 +12,13 @@ import (
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:123@localhost:5432/simple_bank?sslmode=disable"
-)
 
 func TestMain(m *testing.M){
-	var err error
-	testDB,err = sql.Open(dbDriver, dbSource)
+	config,err := util.LoadConfig("../..")
+	if err != nil{
+		log.Fatal("cannot load config:",err)
+	}
+	testDB,err = sql.Open(config.DBDriver, config.DBSource)
 	//defer conn.Close()
 	if err != nil{
 		log.Fatal("cannot connect to db: ",err)
